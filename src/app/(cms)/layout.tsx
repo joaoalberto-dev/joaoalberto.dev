77dc9e7f61a3642741fbf1eddc0f427d9b42e262
João Alberto <hey@joaoalberto.dev>
2024-07-01 14:42:43 -0300

    fix: outstatic layout

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -6,8 +6,6 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <div>{children}</div>
   );
 }
