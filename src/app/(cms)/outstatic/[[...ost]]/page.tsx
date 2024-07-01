dd711c392669ad488e494c7bab1328dd0d04631a
João Alberto <hey@joaoalberto.dev>
2024-07-01 15:15:51 -0300

    fix(layout): by passing root layout padding

--- src/app/(cms)/outstatic/[[...ost]]/page.tsx
+++ src/app/(cms)/outstatic/[[...ost]]/page.tsx
@@ -6,5 +6,9 @@ import { OstClient } from "outstatic/client";
 export default async function Page({ params }: { params: { ost: string[] } }) {
   const ostData = await Outstatic();
 
-  return <OstClient ostData={ostData} params={params} />;
+  return (
+    <div className="fixed inset-0 min-h-[100dvh]">
+      <OstClient ostData={ostData} params={params} />
+    </div>
+  )
 }
