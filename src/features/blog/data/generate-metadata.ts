92ec7a508b7e43fcf5d5f1ce44ccb5bff253cf0b
João Alberto <hey@joaoalberto.dev>
2024-07-01 17:32:55 -0300

    chore(og): improving posts opengraph metadata

--- src/features/blog/data/generate-metadata.ts
+++ src/features/blog/data/generate-metadata.ts
@@ -9,7 +9,26 @@ function generateMetadata({ params }: BlogPageProps): Metadata {
 
   const post = getDocumentBySlug("posts", params.slug, ['title']);
 
-  if (post?.title) metadata.title = post.title
+  if (post?.title) {
+    metadata.title = post.title
+    metadata.openGraph = {
+      title: post.title,
+      description: post.description,
+      url: `https://joaoalberto.dev/blog/${post.slug}`,
+      siteName: 'João Alberto | Software developer',
+      images: [],
+      locale: 'en_US',
+      type: 'website'
+    }
+
+    if (post.coverImage) {
+      metadata.openGraph.images = {
+        url: post.coverImage,
+        width: 800,
+        height: 600,
+      }
+    }
+  }
 
   return metadata
 }
