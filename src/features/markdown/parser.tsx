d358b74010e35f206280913f59d84a2da7806beb
João Alberto <hey@joaoalberto.dev>
2024-07-01 16:20:47 -0300

    feat(zoom): adding the abillity to zoom images

--- src/features/markdown/parser.tsx
+++ src/features/markdown/parser.tsx
@@ -1,8 +1,18 @@
+import { Zoom } from "@/ui/zoom/zoom";
 import { compileMDX } from "next-mdx-remote/rsc";
 
 async function parse(content: string) {
   return await compileMDX({
-    source: content
+    source: content,
+    components: {
+      img(props) {
+        return (
+          <Zoom wrapElement="span">
+            <img {...props} />
+          </Zoom>
+        )
+      }
+    }
   })
 }
 
---
+++ src/ui/zoom/zoom.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+import Zoom from 'react-medium-image-zoom'
+import "react-medium-image-zoom/dist/styles.css"
+
+export { Zoom }
