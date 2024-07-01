896ba89ad73823b9668f7f5c15fb3283cc239099
João Alberto <hey@joaoalberto.dev>
2024-07-01 13:17:02 -0300

    feat: rendering the blog list in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
+import { BlogList } from "@/features/blog-list/ui/blog-list";
 import { Avatar } from "@/ui/avatar/avatar";
 import { Social } from "@/ui/social/social";
 
 export default function Home() {
   return (
-    <main className="flex flex-col p-8 md:p-16 gap-8">
+    <main className="flex flex-col gap-16">
       <Avatar />
       <p className="font-text-title text-3xl font-light">
         Hey, I am João Alberto, a software developer based in Brazil.
-      </p>
+      </p >
       <Social />
-    </main>
+      <BlogList />
+    </main >
   );
 }
