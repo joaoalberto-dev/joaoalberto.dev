9c40b2b8a2e5a086734ffaa148770b05207165fe
João Alberto <hey@joaoalberto.dev>
2024-08-17 13:56:30 -0300

    chore: remove unused code

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,6 @@
-"use client";
-
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 function Navbar() {
-  const path = usePathname();
-
   return (
     <nav className="flex gap-2">
       <Link href="/">/Home</Link>
