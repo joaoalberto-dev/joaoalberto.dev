cd1a211564d3f6e23546bc0f6ba4f0af6fc24d01
João Alberto <hey@joaoalberto.dev>
2024-08-23 02:19:15 -0300

    chore: make all text balance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <main className="pt-8">
       <Title className="mb-8">João Alberto</Title>
-      <p className="md:text-balance mb-4">
+      <p className="text-balance mb-4">
         I am a software developer. Currently I work at{" "}
         <Link href="https://fluency.io/">Fluency Academy</Link> building new
         experiences for language learners.
       </p>
-      <p className="md:text-balance mb-4">
+      <p className="text-balance mb-4">
         I have experience building for real-time applications, e-commerces,
         social networks and real estate companies. You can find more about my
         past on my{" "}
@@ -20,7 +20,7 @@ export default function Home() {
           LinkedIn
         </Link>
       </p>
-      <p className="md:text-balance mb-4">
+      <p className="text-balance mb-4">
         In my spare time, I like to write personal software. You can find more
         on my <Link href="https://github.com/joaoalberto-dev">Github</Link>.
       </p>
