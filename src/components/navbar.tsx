ad1ff804445c5bb4a911bbaeece3b0a037da9687
João Alberto <hey@joaoalberto.dev>
2024-08-23 02:17:01 -0300

    feat: new nav button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,16 @@
+import { IconHome } from "@tabler/icons-react";
 import Link from "next/link";
 
 function Navbar() {
   return (
     <nav className="flex gap-2">
-      <Link href="/">/Home</Link>
+      <Link
+        className="flex gap-2 text-sm p-2 items-center justify-center rounded-md bg-gradient-to-tr from-amber-800 to-amber-500 border border-amber-200 text-amber-50"
+        href="/"
+      >
+        <IconHome className="w-5 h-5" />
+        <span>Home</span>
+      </Link>
     </nav>
   );
 }
