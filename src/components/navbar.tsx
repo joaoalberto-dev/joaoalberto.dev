7d772471f3f2ab4850e0683f21171eaa9f117bc6
João Alberto <hey@joaoalberto.dev>
2024-08-26 20:57:51 -0300

    chore: remove navbar effects

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,11 +5,11 @@ function Navbar() {
   return (
     <nav className="flex gap-2">
       <Link
-        className="flex gap-2 text-sm p-2 items-center justify-center rounded-md bg-gradient-to-tr from-amber-800 to-amber-500 border border-amber-200 text-amber-50"
+        className="flex gap-2 text-sm p-2 items-center justify-center rounded-md hover:bg-white hover:text-amber-800"
         href="/"
       >
         <IconHome className="w-5 h-5" />
-        <span>Home</span>
+        Home
       </Link>
     </nav>
   );
