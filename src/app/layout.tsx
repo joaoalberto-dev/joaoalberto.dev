21e65f9ff5befdc71671745c1cd9262893e37543
João Alberto <hey@joaoalberto.dev>
2024-09-22 03:27:26 -0300

    chore: change from mousemove to pointermove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="overscroll-none" lang="en">
       <head>
         <meta
           name="viewport"
