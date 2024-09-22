f6bbfef84e87156ba5084679fc762c0a6ca1d34c
João Alberto <hey@joaoalberto.dev>
2024-09-22 03:13:14 -0300

    feature: playing with canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,36 @@
+import { Canvas } from "@/components/canvas";
 import { Link } from "@/components/link";
 import { Title } from "@/components/title";
 
 export default function Home() {
   return (
-    <main>
-      <Title>João Alberto</Title>
-      <div className="space-y-2 opacity-70 mt-8">
-        <p className="leading-loose">I am a software developer.</p>
-        <p className="leading-loose">
-          Currently I work at&nbsp;
-          <Link href="https://fluency.io/">Fluency Academy</Link> building new
-          experiences for language learners.
-        </p>
-        <p className="leading-loose">
-          I have experience building real-time applications, e-commerces, social
-          networks and real estate companies. You can find more about my past on
-          my&nbsp;
-          <Link href="https://www.linkedin.com/in/joaoalberto-dev/">
-            LinkedIn
-          </Link>
-        </p>
-        <p className="leading-loose">
-          In my spare time, I like to write personal software. You can find more
-          on my <Link href="https://github.com/joaoalberto-dev">Github</Link>.
-        </p>
+    <main className="relative">
+      <div className="fixed z-0 inset-0">
+        <Canvas />
+      </div>
+      <div className="relative z-10">
+        <Title>João Alberto</Title>
+        <div className="space-y-2 opacity-70 mt-8">
+          <p className="leading-loose">I am a software developer.</p>
+          <p className="leading-loose">
+            Currently I work at&nbsp;
+            <Link href="https://fluency.io/">Fluency Academy</Link> building new
+            experiences for language learners.
+          </p>
+          <p className="leading-loose">
+            I have experience building real-time applications, e-commerces,
+            social networks and real estate companies. You can find more about
+            my past on my&nbsp;
+            <Link href="https://www.linkedin.com/in/joaoalberto-dev/">
+              LinkedIn
+            </Link>
+          </p>
+          <p className="leading-loose">
+            In my spare time, I like to write personal software. You can find
+            more on my{" "}
+            <Link href="https://github.com/joaoalberto-dev">Github</Link>.
+          </p>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/canvas.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+function Canvas() {
+  const mouse = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const canvas = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const SCREEN_WIDTH = window.innerWidth;
+    const SCREEN_HEIGHT = window.innerHeight;
+
+    if (!canvas.current) return;
+
+    canvas.current.width = SCREEN_WIDTH;
+    canvas.current.height = SCREEN_HEIGHT;
+
+    const context = canvas.current.getContext("2d");
+
+    if (!context) return;
+
+    function render(context: CanvasRenderingContext2D) {
+      context.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+      for (let row = 0; row <= SCREEN_HEIGHT / 10; row++) {
+        for (let col = 0; col <= SCREEN_WIDTH / 10; col++) {
+          const X = col * 10 + 2.5;
+          const Y = row * 10 + 2.5;
+
+          context.beginPath();
+          context.fillStyle = "rgba(255,255,255,.1)";
+          context.moveTo(col * 10 + 2.5, row * 10 + 2.5);
+          context.arc(X, Y, 1, 0, Math.PI * 2);
+          context.closePath();
+
+          if (Math.random() > 0.95) context.fillStyle = "rgba(255,255,255,.7)";
+
+          const a = X - mouse.current.x;
+          const b = Y - mouse.current.y;
+          const c = Math.sqrt(a * a + b * b);
+
+          if (c < 100) {
+            context.fillStyle = "rgba(255,255,255,0)";
+          }
+
+          context.fill();
+        }
+      }
+
+      setTimeout(() => {
+        requestAnimationFrame(() => render(context));
+      }, 1000 / 24);
+    }
+
+    render(context);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (event) => {
+      mouse.current = { x: event.clientX, y: event.clientY };
+    });
+
+    return () => {
+      window.removeEventListener("mousemove", (event) => {
+        mouse.current = { x: event.clientX, y: event.clientY };
+      });
+    };
+  });
+
+  return <canvas ref={canvas} className="w-dvw h-dvh" />;
+}
+
+export { Canvas };
