399e263c76c7bed1146d6886350327c4f742396c
João Alberto <hey@joaoalberto.dev>
2024-10-11 19:08:46 -0300

    chore: render home title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,12 @@
-import { Canvas } from "@/components/canvas";
-import { Link } from "@/components/link";
-import { Title } from "@/components/title";
+import { HomeDescription } from "@/components/home/description";
+import { HomeTitle } from "@/components/home/title";
 
 export default function Home() {
   return (
-    <main className="relative">
-      <div className="fixed z-0 inset-0">
-        <Canvas />
-      </div>
-      <div className="relative z-10 select-none">
-        <Title>
-          <Link href="/">João Alberto</Link>
-        </Title>
-        <div className="space-y-2 opacity-70 mt-8">
-          <p className="leading-loose">I am a software developer.</p>
-          <p className="leading-loose">
-            Currently I work at&nbsp;
-            <Link href="https://fluency.io/">Fluency Academy</Link> building new
-            experiences for language learners.
-          </p>
-          <p className="leading-loose">
-            I have experience building real-time applications, e-commerces,
-            social networks and real estate companies. You can find more about
-            my past on my&nbsp;
-            <Link href="https://www.linkedin.com/in/joaoalberto-dev/">
-              LinkedIn
-            </Link>
-          </p>
-          <p className="leading-loose">
-            In my spare time, I like to write personal software. You can find
-            more on my{" "}
-            <Link href="https://github.com/joaoalberto-dev">Github</Link>.
-          </p>
-        </div>
+    <main className="min-w-dvw max-h-dvh p-4 md:p-16">
+      <div className="min-h-[calc(100dvh-32px)] md:min-h-[calc(100dvh-128px)] justify-end items-start flex flex-col">
+        <HomeTitle />
+        <HomeDescription />
       </div>
     </main>
   );
