cf0f87dcfb28060f6655987ede4a3eb1e0b8ddf4
João Alberto <hey@joaoalberto.dev>
2024-10-12 02:04:42 -0300

    feat: commit-list ui and data

---
+++ src/features/github/data/get-latest-commits.ts
@@ -0,0 +1,19 @@
+import { Commit } from "../types";
+
+async function getLatestCommits(): Promise<Commit[]> {
+  const response = await fetch(
+    "https://api.github.com/repos/joaoalberto-dev/joaoalberto.dev/commits?per_page=10",
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: "Bearer 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",
+        "X-Github-Api-Version": "2022-11-28",
+      },
+    }
+  );
+  const commits = await response.json();
+
+  return commits;
+}
+
+export { getLatestCommits };
---
+++ src/features/github/ui/commit-list.tsx
@@ -0,0 +1,31 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Commit } from "../types";
+import Link from "next/link";
+
+type CommitListProps = {
+  commits: Commit[];
+};
+
+export default function CommitList({ commits }: CommitListProps) {
+  return (
+    <div className="mt-16 space-y-4 col-start-2 row-start-1 lg:h-[100dvh] lg:pt-16 lg:m-0 lg:overflow-y-scroll row-span-2 hide-scrollbar lg:pb-16">
+      <h3 className="text-blue-700">Latest Github activity</h3>
+      {commits.map(({ html_url, sha, commit }) => (
+        <Card key={sha} className="w-full shadow-none">
+          <CardContent className="p-4">
+            <Link href={html_url}>
+              <div className="flex items-start space-x-2">
+                <div className="flex-grow space-y-2">
+                  <p className="text-sm font-medium">{commit.message}</p>
+                  <p className="text-xs text-muted-foreground">
+                    Commit: {sha.substring(0, 7)}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
