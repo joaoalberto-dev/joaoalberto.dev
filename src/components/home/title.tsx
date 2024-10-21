13097628333cf88097d22664023aa83b24487197
João Alberto <hey@joaoalberto.dev>
2024-10-21 20:01:39 -0300

    refactor: home title component

--- src/components/home/title.tsx
+++ src/components/home/title.tsx
@@ -1,30 +1,30 @@
 "use client";
 
 import {
-  motion,
-  useMotionTemplate,
-  useScroll,
-  useTransform,
+	motion,
+	useMotionTemplate,
+	useScroll,
+	useTransform,
 } from "framer-motion";
 import { useRef } from "react";
 
 function HomeTitle() {
-  const container = useRef<HTMLHeadingElement>(null);
-  const { scrollY } = useScroll({ target: container });
-  const scaleFontSize = useTransform(scrollY, [0, 200], [48, 24]);
-  const titleFontSize = useMotionTemplate`${scaleFontSize}px`;
+	const container = useRef<HTMLHeadingElement>(null);
+	const { scrollY } = useScroll({ target: container });
+	const scaleFontSize = useTransform(scrollY, [0, 200], [48, 24]);
+	const titleFontSize = useMotionTemplate`${scaleFontSize}px`;
 
-  return (
-    <motion.h1
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      style={{ fontSize: titleFontSize }}
-      ref={container}
-      className="col-start-1 row-start-1 lg:m-0 bg-[rgba(255,255,255,.4)] mt-[35vh] w-full font-title backdrop-blur-sm py-4 text-blue-700 sticky top-0 lg:self-end"
-    >
-      João Alberto
-    </motion.h1>
-  );
+	return (
+		<motion.h1
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			style={{ fontSize: titleFontSize }}
+			ref={container}
+			className="px-4 -ml-4 col-start-1 row-start-1 lg:m-0 bg-white opacity-10 mt-[35vh] w-[calc(100%+32px)] font-title py-4 text-blue-700 sticky top-0 lg:self-end"
+		>
+			João Alberto
+		</motion.h1>
+	);
 }
 
 export { HomeTitle };
