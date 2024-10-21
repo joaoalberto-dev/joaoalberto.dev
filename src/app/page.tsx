bc4ffe8aa0f0777c1eb88453958bbeb4733c4373
João Alberto <hey@joaoalberto.dev>
2024-10-21 19:47:27 -0300

    feat: render the new project area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HomeDescription } from "@/components/home/description";
 import { HomeTitle } from "@/components/home/title";
 import { getLatestCommits } from "@/features/github/data/get-latest-commits";
 import CommitList from "@/features/github/ui/commit-list";
-import { HOUR_IN_SECONDS } from "@/lib/time";
+import ProjectsList from "@/features/projects/ui/projects-list";
 
 async function Home() {
   const commits = await getLatestCommits();
@@ -11,11 +11,14 @@ async function Home() {
     <main className="min-w-dvw p-4 lg:px-16 lg:py-0 lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:h-dvh">
       <HomeTitle />
       <HomeDescription />
-      <CommitList commits={commits} />
+      <div className="row-span-2">
+        <ProjectsList />
+        <CommitList commits={commits} />
+      </div>
     </main>
   );
 }
 
-export const revalidate = HOUR_IN_SECONDS;
+export const revalidate = 3600;
 
 export default Home;
