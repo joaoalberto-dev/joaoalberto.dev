74edf020dab039593757d399ddb5d482073ac284
João Alberto <hey@joaoalberto.dev>
2024-11-14 14:50:42 -0300

    chore: hidde project scrollbar

--- src/features/projects/ui/projects-list.tsx
+++ src/features/projects/ui/projects-list.tsx
@@ -3,35 +3,35 @@ import VideoProject from "./video-project";
 import { ProjectWrapper } from "./wrapper";
 
 const projects: Project[] = [
-	{
-		title: "Robinhood animation built in canvas",
-		description: "",
-		kind: "video",
-		url: "https://canvas.joaoalberto.dev/robinhood",
-		videoUrl: "/videos/robinhood.webm",
-	},
+  {
+    title: "Robinhood animation built in canvas",
+    description: "",
+    kind: "video",
+    url: "https://canvas.joaoalberto.dev/robinhood",
+    videoUrl: "/videos/robinhood.webm",
+  },
 ];
 
 export default function ProjectsList() {
-	return (
-		<div className="mt-16 space-y-4 col-start-2 row-start-1 lg:pt-16 lg:m-0 lg:overflow-y-scroll lg:pb-16">
-			<div className="flex items-end justify-between">
-				<h3 className="text-blue-700">Projects</h3>
-			</div>
-			<div className="flex flex-wrap text-gray-400 gap-[4%]">
-				{projects.map((project) => {
-					let content = null;
+  return (
+    <div className="mt-16 space-y-4 col-start-2 row-start-1 lg:pt-16 lg:m-0 lg:overflow-y-scroll lg:pb-16 hide-scrollbar">
+      <div className="flex items-end justify-between">
+        <h3 className="text-blue-700">Projects</h3>
+      </div>
+      <div className="flex flex-wrap text-gray-400 gap-[4%]">
+        {projects.map((project) => {
+          let content = null;
 
-					if (project.kind === "video")
-						content = <VideoProject project={project} />;
+          if (project.kind === "video")
+            content = <VideoProject project={project} />;
 
-					return (
-						<ProjectWrapper url={project.url} key={project.url}>
-							{content}
-						</ProjectWrapper>
-					);
-				})}
-			</div>
-		</div>
-	);
+          return (
+            <ProjectWrapper url={project.url} key={project.url}>
+              {content}
+            </ProjectWrapper>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
