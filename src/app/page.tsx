fc823dcff5c9f12df35be8ad98e13152a436d51c
João Alberto <hey@joaoalberto.dev>
2024-11-26 01:40:11 -0300

    chore: render the new layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 import { HomeDescription } from "@/components/home/description";
 import { HomeTitle } from "@/components/home/title";
+import { BasicPageLayout } from "@/components/layouts/basic-page-layout";
 import CommitList from "@/features/github/ui/commit-list";
 import ProjectsList from "@/features/projects/ui/projects-list";
 
 async function Home() {
   return (
-    <main className="min-w-dvw p-4 lg:px-16 lg:py-0 lg:flex lg:justify-evenly lg:h-dvh lg:gap-8">
-      <div className="lg:flex-none lg:max-w-[64ch] lg:flex lg:items-center lg:justify-center flex-col">
-        <HomeTitle />
-        <HomeDescription />
-      </div>
-      <div className="lg:flex-none lg:max-w-[64ch] lg:h-dvh overflow-auto hide-scrollbar">
-        <ProjectsList />
-        <CommitList />
-      </div>
-    </main>
+    <BasicPageLayout
+      mainContent={
+        <>
+          <HomeTitle />
+          <HomeDescription />
+        </>
+      }
+      sideContent={
+        <>
+          <ProjectsList />
+          <CommitList />
+        </>
+      }
+    ></BasicPageLayout>
   );
 }
 
