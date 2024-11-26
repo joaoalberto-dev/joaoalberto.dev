6e5c825cebd1f29ac90187fce69d4e5a670468d7
João Alberto <hey@joaoalberto.dev>
2024-11-26 02:38:34 -0300

    refactor: project video player

--- src/features/projects/ui/video-project.tsx
+++ src/features/projects/ui/video-project.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import type { SyntheticEvent } from "react";
+import { PauseIcon, PlayIcon } from "lucide-react";
+import { useRef, useState, type SyntheticEvent } from "react";
 import type { Project } from "../types";
 
 type VideoProjectProps = {
@@ -10,26 +11,51 @@ type VideoProjectProps = {
 };
 
 export default function VideoProject({ project }: VideoProjectProps) {
-  function play(event: SyntheticEvent<HTMLVideoElement, PointerEvent>) {
-    event.currentTarget.play();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  function play(event: SyntheticEvent<HTMLVideoElement | HTMLButtonElement>) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    videoRef.current?.play();
+    setIsPlaying(true);
   }
 
-  function pause(event: SyntheticEvent<HTMLVideoElement>) {
-    event.currentTarget.pause();
+  function pause(event: SyntheticEvent<HTMLVideoElement | HTMLButtonElement>) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    videoRef.current?.pause();
+    setIsPlaying(false);
   }
 
   return (
     <>
-      <video
-        src={project.videoUrl}
-        className="w-full aspect-video rounded-md"
-        controls={false}
-        autoPlay={false}
-        onPointerEnter={play}
-        onPointerLeave={pause}
-        loop
-        muted
-      />
+      <div className="relative aspect-video">
+        <video
+          ref={videoRef}
+          src={project.videoUrl}
+          className="w-full aspect-video rounded-md"
+          controls={false}
+          autoPlay={false}
+          onPointerEnter={play}
+          onPointerLeave={pause}
+          loop
+          muted
+        />
+        <button
+          type="button"
+          onClick={isPlaying ? pause : play}
+          className="absolute lg:hidden right-2 bottom-2 w-8 h-8 bg-white/30 rounded-full flex items-center justify-center"
+        >
+          {isPlaying ? (
+            <PauseIcon className="w-4" />
+          ) : (
+            <PlayIcon className="w-4" />
+          )}
+        </button>
+      </div>
       <h3 className="font-semibold">{project.title}</h3>
     </>
   );
