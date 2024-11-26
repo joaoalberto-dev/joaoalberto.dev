3569956e3ecf7c87716a6b3253acd29a1585bc02
João Alberto <hey@joaoalberto.dev>
2024-11-26 00:20:43 -0300

    chore: make title sticky with framer-motion

--- src/components/home/title.tsx
+++ src/components/home/title.tsx
@@ -11,18 +11,20 @@ import { useRef } from "react";
 function HomeTitle() {
   const container = useRef<HTMLHeadingElement>(null);
   const { scrollY } = useScroll({ target: container });
-  const scaleXPadding = useTransform(scrollY, [0, 200], [0, 16]);
-  const xPadding = useMotionTemplate`${scaleXPadding}px`
+
   const scaleFontSize = useTransform(scrollY, [0, 200], [48, 24]);
-  const titleFontSize = useMotionTemplate`${scaleFontSize}px`;
+  const fontSize = useMotionTemplate`${scaleFontSize}px`;
+
+  const topPosition = useTransform(scrollY, [0, 200], [200, 0]);
+  const top = useMotionTemplate`${topPosition}px`;
 
   return (
     <motion.h1
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      style={{ fontSize: titleFontSize, paddingInline: xPadding }}
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      style={{ fontSize, top, originX: 0, originY: 0.5 }}
       ref={container}
-      className="t-0 mt-[35vh] p-4 rounded-full bg-brand/50 backdrop-blur align-baseline leading-none font-title text-blue-700 sticky top-[16px]"
+      className="fixed p-4 left-0 right-0 bg-brand/50 backdrop-blur font-title text-blue-700"
     >
       João Alberto
     </motion.h1>
