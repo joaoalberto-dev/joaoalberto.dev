dc4ca9818965cfd9d56fbed8e88a66081d2ba75c
João Alberto <hey@joaoalberto.dev>
2024-11-28 02:16:21 -0300

    feat: adding utils for fetching blog posts list and by id

---
+++ src/features/blog/data/get-blog-posts.ts
@@ -0,0 +1,22 @@
+import { contentfulClient } from "@/lib/contentful";
+import { BlogPost } from "../types";
+
+type ContentfulBlogPost = {
+  fields: BlogPost;
+  contentTypeId: string;
+}
+
+export async function getBlogPosts() {
+  const blogPosts = await contentfulClient.getEntries<ContentfulBlogPost>({
+    content_type: "blogPost",
+  });
+  
+  const items = blogPosts.items.map(({ sys,fields }) => {
+    return {
+      ...fields,
+      id: sys.id,
+    } as BlogPost;
+  });
+
+  return items;
+}
