900a83de76c3a5600898a140802f9e07cf965fb9
João Alberto <hey@joaoalberto.dev>
2025-01-11 18:27:13 -0300

    feat: header component

---
+++ src/core/components/header/header.tsx
@@ -0,0 +1,19 @@
+import text from "@/core/styles/text.module.css";
+import styles from "./header.module.css";
+
+type HeaderProps = {
+  title: string;
+  subtitle: string;
+}
+
+function Header({ title, subtitle }: HeaderProps) {
+  return (
+    <header className={styles.container}>
+      <h1 className={text.title}>{title}</h1>
+      <p className={text.paragraph}>{subtitle}</p>
+    </header>
+  )
+}
+
+export { Header };
+
