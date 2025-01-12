1ff939c8a63f5e7dd7e1bea705ed28e40eb0b087
João Alberto <hey@joaoalberto.dev>
2025-01-11 22:58:35 -0300

    feat: new experiences component

---
+++ src/core/components/experiences/experiences.tsx
@@ -0,0 +1,27 @@
+import text from "@/core/styles/text.module.css";
+import styles from "./experiences.module.css";
+
+const experiences = [
+  { name: "Fluency Academy", time: "Mar 2022 → now" },
+  { name: "PixelWolf", time: "Mar 2020 → Mar 2022" },
+  { name: "MadeiraMadeira", time: "Jul 2019 → Dec 2019" },
+  { name: "Critical Mass", time: "Oct 2017 → Nov 2020" },
+  { name: "Personaliza", time: "Jul 2015 → Dec 2017" },
+  { name: "Volts Digital", time: "Feb 2014 → Jul 2014" }
+];
+
+function Experiences() {
+  return (
+    <ul className={styles.container}>
+      {experiences.map(({ name, time }) => (
+        <li className={styles.item} key={name}>
+          <h4 className={text.paragraph}>{name}</h4>
+          <hr className={styles.line} />
+          <p className={text.small}>{time}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export { Experiences };
