99644e1b5be1434e6f3ddc22bcb76a4f49f76fbe
João Alberto <hey@joaoalberto.dev>
2025-01-11 22:33:11 -0300

    feat: new project-list component

---
+++ src/core/components/project-list/project-list.tsx
@@ -0,0 +1,25 @@
+import { Project } from "../project/project";
+import styles from "./project-list.module.css";
+
+const projects = [
+  {
+    name: "mini-video-editor",
+    href: "https://github.com/joaoalberto-dev/mini-video-editor",
+  }
+];
+
+function ProjectList() {
+  return (
+    <div className={styles.project_list}>
+      {projects.map((project) => {
+        return (
+          <Project key={project.href} href={project.href}>
+            {project.name}
+          </Project>
+        );
+      })}
+    </div>
+  );
+}
+
+export { ProjectList };
