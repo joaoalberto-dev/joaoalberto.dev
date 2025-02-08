a8c044adb31b16b284c4518b3db53cfc873306f6
João Alberto <hey@joaoalberto.dev>
2025-02-08 16:24:07 -0300

    fix: sitemap dir

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,8 @@
-import { promises as fs } from "fs";
+import { getBlogSlugs } from "@/core/utils/get-blog-slugs";
 import path from "path";
 
-async function getBlogSlugs(dir: string) {
-  const entries = await fs.readdir(dir, {
-    recursive: true,
-    withFileTypes: true,
-  });
-  return entries
-    .filter((entry) => entry.isFile() && entry.name === "page.mdx")
-    .map((entry) => {
-      const relativePath = path.relative(
-        dir,
-        path.join(entry.parentPath, entry.name)
-      );
-      return path.dirname(relativePath);
-    })
-    .map((slug) => slug.replace(/\\/g, "/"));
-}
-
 export default async function sitemap() {
-  const blogDirectory = path.join(process.cwd(), "app", "blog");
+  const blogDirectory = path.join(process.cwd(), "src", "app", "blog");
   const slugs = await getBlogSlugs(blogDirectory);
 
   const blogPosts = slugs.map((slug) => ({
