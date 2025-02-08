20475914dd6d133e35ffb13fd4ca75f9b2d67fe3
João Alberto <hey@joaoalberto.dev>
2025-02-08 16:25:26 -0300

    fix: add missing file

---
+++ src/core/utils/get-blog-slugs.ts
@@ -0,0 +1,21 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+async function getBlogSlugs(dir: string) {
+  const entries = await fs.readdir(dir, {
+    recursive: true,
+    withFileTypes: true,
+  });
+  return entries
+    .filter((entry) => entry.isFile() && entry.name === "page.mdx")
+    .map((entry) => {
+      const relativePath = path.relative(
+        dir,
+        path.join(entry.parentPath, entry.name)
+      );
+      return path.dirname(relativePath);
+    })
+    .map((slug) => slug.replace(/\\/g, "/"));
+}
+
+export { getBlogSlugs };
