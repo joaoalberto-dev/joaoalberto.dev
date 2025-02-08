8bd1c549997b16213aa4a5b742901c64cf585a77
João Alberto <hey@joaoalberto.dev>
2025-02-08 17:53:39 -0300

    chore: navigate back

--- src/core/components/layout/layout.tsx
+++ src/core/components/layout/layout.tsx
@@ -1,14 +1,30 @@
-import { ReactNode } from "react"
-import layout from "./layout.module.css"
+"use client";
+
+import { ReactNode } from "react";
+import layout from "./layout.module.css";
+import { usePathname } from "next/navigation";
+import { Link } from "next-view-transitions";
+import { LeftIcon } from "../icons";
 
 type LayoutProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
-function Layout({children}: LayoutProps) {
+function Layout({ children }: LayoutProps) {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+
   return (
-    <div className={layout.container}>{children}</div>
-  )
+    <div className={layout.container}>
+      {!isHome && (
+        <nav className={layout.nav}>
+          <Link href="/">
+            <LeftIcon />
+          </Link>
+        </nav>
+      )}
+      <main>{children}</main>
+    </div>
+  );
 }
-
-export { Layout }
+export { Layout };
