0306154fc07fab71edf8526f6b95c1a1ea632ecd
João Alberto <hey@joaoalberto.dev>
2025-03-28 00:57:34 -0300

    chore: extract the bleed css to its own classname

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -1,5 +1,6 @@
 import text from "@/core/styles/text.module.css";
 import styles from "./header.module.css";
+import { cn } from "@/core/utils/cn";
 
 type HeaderProps = {
   title: string;
@@ -8,7 +9,7 @@ type HeaderProps = {
 
 function Header({ title, subtitle }: HeaderProps) {
   return (
-    <header className={styles.container}>
+    <header className={cn(styles.container, "bleed")}>
       <h1 className={text.title}>{title}</h1>
       {subtitle && <p className={text.paragraph}>{subtitle}</p>}
     </header>
