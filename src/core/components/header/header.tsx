026cf3cd4536f9d5550d64a8be228d509b42337d
João Alberto <hey@joaoalberto.dev>
2025-04-03 01:59:08 -0300

    chore: improve header styles and render new canvas background component

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import text from "@/core/styles/text.module.css";
 import styles from "./header.module.css";
+import { useRef } from "react";
 import { cn } from "@/core/utils/cn";
+import { CanvasBackground } from "../canvas-background/canvas-background";
 
 type HeaderProps = {
   title: string;
@@ -8,10 +12,13 @@ type HeaderProps = {
 };
 
 function Header({ title, subtitle }: HeaderProps) {
+  const parentRef = useRef<HTMLDivElement>(null);
+
   return (
-    <header className={cn(styles.container, "bleed")}>
+    <header className={cn(styles.container, "header", "bleed")} ref={parentRef}>
       <h1 className={text.title}>{title}</h1>
       {subtitle && <p className={text.paragraph}>{subtitle}</p>}
+      <CanvasBackground parentRef={parentRef} />
     </header>
   );
 }
