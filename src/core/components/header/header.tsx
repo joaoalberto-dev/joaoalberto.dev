9619388d37976a69acb615431e122188810e6d8b
João Alberto <hey@joaoalberto.dev>
2025-04-28 21:28:07 -0300

    refactor: removing the canvas component from the header

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -2,9 +2,7 @@
 
 import text from "@/core/styles/text.module.css";
 import styles from "./header.module.css";
-import { useRef } from "react";
 import { cn } from "@/core/utils/cn";
-import { CanvasBackground } from "../canvas-background/canvas-background";
 
 type HeaderProps = {
   title: string;
@@ -12,13 +10,10 @@ type HeaderProps = {
 };
 
 function Header({ title, subtitle }: HeaderProps) {
-  const parentRef = useRef<HTMLDivElement>(null);
-
   return (
-    <header className={cn(styles.container, "header", "bleed")} ref={parentRef}>
+    <header className={cn(styles.container, "header", "bleed")}>
       <h1 className={text.title}>{title}</h1>
       {subtitle && <p className={text.paragraph}>{subtitle}</p>}
-      <CanvasBackground parentRef={parentRef} />
     </header>
   );
 }
