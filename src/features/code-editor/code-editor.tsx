390c3b30f56ef0968cd1b1ed02bfd9606c145d29
João Alberto <hey@joaoalberto.dev>
2025-06-28 12:26:43 -0300

    feat: basic code editor component based com monaco-editor

---
+++ src/features/code-editor/code-editor.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useColorMode } from "@/core/hooks/use-color-mode";
+import Editor from "@monaco-editor/react";
+import ghDark from "monaco-themes/themes/GitHub Dark.json";
+import ghLight from "monaco-themes/themes/GitHub Light.json";
+import styles from "./code-editor.module.css";
+
+const code = `function x() {
+  console.log("Hello world!");
+}`;
+
+function CodeEditor() {
+  const mode = useColorMode();
+
+  const beforeMount = (monaco: typeof import("monaco-editor")) => {
+    if (monaco?.editor) {
+      monaco.editor.defineTheme("gh-light", {
+        ...ghLight,
+        base: "vs",
+      });
+
+      monaco.editor.defineTheme("gh-dark", {
+        ...ghDark,
+        base: "vs-dark",
+      });
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Editor
+        theme={mode === "dark" ? "gh-dark" : "gh-light"}
+        options={{
+          scrollBeyondLastColumn: 0,
+          scrollBeyondLastLine: false,
+          renderLineHighlight: "none",
+          lineHeight: 30,
+          padding: {
+            top: 20,
+            bottom: 20,
+          },
+          minimap: {
+            enabled: false,
+          },
+        }}
+        defaultLanguage="javascript"
+        defaultValue={code}
+        beforeMount={beforeMount}
+      />
+    </div>
+  );
+}
+
+export { CodeEditor };
