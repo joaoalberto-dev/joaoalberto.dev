e42efc9a0e561ac3fde529b8893679e529a74021
João Alberto <hey@joaoalberto.dev>
2025-07-19 00:45:31 -0300

    chore: redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,113 @@
-import Image from "next/image";
+import Link from "next/link";
+import { Github, Linkedin, RightArrow, X } from "@/core/icons";
+
+const socials = [
+  { Image: Github, name: "Github", link: "https://github.com/joaoalberto-dev" },
+  {
+    Image: Linkedin,
+    name: "Linkedin",
+    link: "https://www.linkedin.com/in/joao-alberto-mo/",
+  },
+  { Image: X, name: "X", link: "https://x.com/joaoalbertodev" },
+];
+
+const projects = [
+  {
+    name: "My portfolio",
+    link: "https://github.com/joaoalberto-dev/joaoalberto.dev",
+  },
+];
+
+const blogs = [
+  {
+    title: "Tips for engineers",
+    link: "/blog/tips-for-engineers",
+    description:
+      "This is a list of some thoughts I collected over the years about how to be a good software engineer.",
+  },
+];
+
+const experiences = [
+  { name: "Fluency Academy", time: "Mar 2022 → Now" },
+  { name: "PixelWolf", time: "Mar 2020 → Mar 2022" },
+  { name: "MadeiraMadeira", time: "Jul 2019 → Dec 2019" },
+  { name: "Critical Mass", time: "Oct 2017 → Nov 2020" },
+  { name: "Personaliza", time: "Jul 2015 → Dec 2017" },
+  { name: "Volts Digital", time: "Feb 2014 → Jul 2014" },
+];
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className="flex flex-col gap-y-16 max-w-[420px] mx-auto">
+      <header className="flex flex-col gap-4">
+        <h1 className="font-sans leading-4 font-bold text-amber-950">
+          João Alberto
+        </h1>
+        <p className="opacity-50 font-serif leading-5">
+          I'm a Brazilian software engineer interested in all aspects of
+          computing and human–computer interaction.
+          <br />
+          <br />I have over 10 years of experience building software that lasts,
+          scales, and delights users, from simple blogs to real-time
+          applications.
+        </p>
+      </header>
+      <nav className="flex gap-8">
+        {socials.map(({ link, Image, name }) => (
+          <Link href={link} key={link} target="_blank" title={name}>
+            <Image />
+            <span className="sr-only">{name}</span>
+          </Link>
+        ))}
+      </nav>
+      <section className="flex flex-col gap-8">
+        <h2 className="font-semibold leading-4 opacity-80">Projects</h2>
+        <ul className="flex flex-col gap-2">
+          {projects.map((project) => (
+            <li key={project.link}>
+              <Link
+                className="-ml-4 outline-0 font-sans text-base leading-4 w-[calc(100%+32px)] flex justify-between group focus:bg-amber-100 hover:bg-amber-100 px-4 py-4 items-center rounded-lg border border-transparent focus:border-amber-800/20 hover:border-amber-800/20"
+                href={project.link}
+                target="_blank"
+              >
+                <span>{project.name}</span>
+                <span className="hidden group-focus:inline group-hover:inline">
+                  <RightArrow />
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="flex flex-col gap-8">
+        <h2 className="font-semibold leading-4 opacity-80">Blog</h2>
+        <ul className="flex flex-col gap-2">
+          {blogs.map((blog) => (
+            <li key={blog.link}>
+              <Link className="flex flex-col gap-2" href={blog.link}>
+                <span>{blog.title}</span>
+                <span className="font-serif leading-5 opacity-50">
+                  {blog.description}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="flex flex-col gap-8">
+        <h2 className="font-semibold leading-4 opacity-80">Experiences</h2>
+        <ul className="flex flex-col gap-2">
+          {experiences.map((experience) => (
+            <li
+              className="flex justify-between items-center"
+              key={experience.name}
+            >
+              <span>{experience.name}</span>
+              <span className="font-serif opacity-50">{experience.time}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
   );
 }
---
+++ src/core/icons/index.ts
@@ -0,0 +1,7 @@
+import { RightArrow } from "./right-arrow";
+
+import { Github } from "./github";
+import { Linkedin } from "./linkedin";
+import { X } from "./x";
+
+export { Github, Linkedin, X, RightArrow };
