e9d20fd787d4df9071b61169fccf4f5c29837ebd
João Alberto <hey@joaoalberto.dev>
2025-10-03 23:18:03 -0300

    chore: using Google Sans Code font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Google Sans Code",
+        cssVariable: "--font-body",
+      },
+    ],
+  },
+
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
