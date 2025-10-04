538db03d5e5d25da7145488dedf9c19c47c5023a
João Alberto <hey@joaoalberto.dev>
2025-10-04 01:58:05 -0300

    chore: rendering the new grid background on canvas

--- src/components/grid-background/grid-background.tsx
+++ src/components/grid-background/grid-background.tsx
@@ -1,12 +1,20 @@
-import type { ReactNode } from "react";
+import { type ReactNode, useRef } from "react";
+import { useGridBackground } from "./hooks/use-grid-background";
+import { useScreenSize } from "./hooks/use-screen-size";
 
 type GridBackgroundProps = {
   children: ReactNode;
 };
 
 export function GridBackground({ children }: GridBackgroundProps) {
+  const size = useScreenSize();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useGridBackground(canvasRef, size);
+
   return (
     <div className="min-w-full min-h-screen relative">
+      <canvas ref={canvasRef} className="absolute inset-0 z-0" {...size} />
       <main className="relative z-10">{children}</main>
     </div>
   );
