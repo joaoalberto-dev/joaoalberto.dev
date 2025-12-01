910d27d519c7ec7f39a39be9790771fcdfc285db
João Alberto <hey@joaoalberto.dev>
2025-12-01 20:47:51 -0300

    feat(bookmarks): adding new bookmarks section

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { glob, file } from "astro/loaders";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/posts" }),
@@ -10,4 +10,17 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const bookmarks = defineCollection({
+  type: "data",
+  schema: z.object({
+    bookmarks: z.array(
+      z.object({
+        title: z.string(),
+        link: z.string(),
+        category: z.string(),
+      })
+    ),
+  }),
+});
+
+export const collections = { blog, bookmarks };
